a7993701cf8897daf56a8e9b9949eca8352cdbad
0ette <attepaananen2@gmail.com>
2019-11-13 09:18:35 +0000

    add all

---
+++ codeigniter/application/views/netti_sivu.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+<title>MouseTrap</title>
+</head>
+
+<body bgcolor=DDDDDD>
+
+<h1 align=center>Mouse Trap</h1>
+
+<input style="height:200px;width:200px" type="reset" value="Ota Kuva" name="kuva" />
+
+
+
+
+</body>
+
+</html>
+
